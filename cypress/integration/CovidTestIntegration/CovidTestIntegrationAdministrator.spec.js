3327a4c0a5d8267c0c43adaf8c63757d3329b18b
buari SULAIMON <buarisulaimon@gmail.com>
2021-10-17 16:51:02 +0100

    cypress tests completed

--- cypress/integration/CovidTestIntegration/CovidTestIntegrationAdministrator.spec.js
+++ cypress/integration/CovidTestIntegration/CovidTestIntegrationAdministrator.spec.js
@@ -6,49 +6,44 @@ describe('Allocate Booking space', () => {
       cy.get('#capacity').type(500);
       cy.get('#bookingDates').type('2021-10-30');
       cy.get('#submiAllocate').click();
-      cy.wait(2000)
-    });
+      cy.wait(3000)
 
-    it('Book Covid Test', () => {
-      cy.visit('http://localhost:4200/portal/individuals');
-
+      //route to individuals table
+      cy.get('#routeindividuals').click();
+      
       cy.get('#individualFirstName').type('Umar');
       cy.get('#individualLastName').type('Tola');
       cy.get('#individualEmailAddress').type('test9@test.com');
       cy.get('#individualMobileNumber').type('08075213245');
       cy.get('#testType').select('PCRTest');
       cy.get('#location').select('Lagos, Nigeria');
-      cy.get('#bookingDates').type('2021-10-30');
-      
+      cy.get('#bookingDates').type('2021-10-30');     
       cy.get('#submiBooking').click();
       cy.wait(4000)
-    });
 
-    it('Set booking outcome', () => {
-      cy.visit('http://localhost:4200/portal/individualLabs');
+      //route to individual labs
+      cy.get('#routereports').click();
+      cy.get('#routeindividualLabs').click();
 
-      cy.get('#individualEmailAddress').type('test9@test.com');
-      cy.get('#individualMobileNumber').type('08075213245');
+      cy.get('#individualEmailAddresslabs').type('test9@test.com');
+      cy.get('#individualMobileNumberlabs').type('08075213245');
       cy.get('#getBookingDetails').click();
       cy.wait(3000)
-      cy.get('#selectOutComes').select('positive');
+      cy.get('#selectOutComes').select('negative');
       cy.get('#setOutcome').click();
-      cy.wait(2000)
-    });
+      cy.wait(3000)
 
-    it('Cancel Booking', () => {
-      cy.visit('http://localhost:4200/portal/cancelBookings');
+      //route to cancel booking 
+      cy.get('#routecancelBookings').click();
 
       cy.get('#individualEmailAddress').type('test9@test.com');
       cy.get('#individualMobileNumber').type('08075213245');
       cy.get('#getBookingDetails').click();
       cy.get('#canelBooking').click();
       cy.wait(2000)
-    });
 
-    it('Booking Report', () => {
-      cy.visit('http://localhost:4200/portal/reports');
-
+      //route to reports
+      cy.get('#routereports').click();
       cy.get('#bookingStartDates').type('2021-10-30');
       cy.get('#bookingEndDates').type('2021-10-30');
       cy.get('#getReport').click();
@@ -56,7 +51,6 @@ describe('Allocate Booking space', () => {
       cy.get('#viewindividualData').click();
       cy.wait(2000)
     });
-
   });
 
 
